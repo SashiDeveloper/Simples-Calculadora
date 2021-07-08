7d2d39448cfaf29b87da47f46bfa67bfa7338f80
SashiDeveloper <83094605+SashiDeveloper@users.noreply.github.com>
2021-07-07 22:53:40 -0300

    Calculadora

--- calculadora.py
+++ calculadora.py
@@ -27,7 +27,7 @@ elif choice == '2' :
   print(num1, "-", num2, "=", subtraction(num1,num2))
 
 elif choice == '3' :
-  print(num1, "*", num2, "=", multiply(num1,num2))
+  print(num1, "x", num2, "=", multiply(num1,num2))
 
 elif choice == '4' :
   print(num1, "/", num2, "=", divide(num1,num2))
