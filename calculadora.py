aa1d4bf9321afb310664f9fe38d96fe7fbc055f5
SashiDeveloper <83094605+SashiDeveloper@users.noreply.github.com>
2021-07-08 08:58:55 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -24,7 +24,7 @@ if choice == '1' :
   print(num1, "+", num2, "=", add(num1,num2))
 
 elif choice == '2' :
-  print(num1, "-", num2, "=", subtraction(num1,num2))
+  print(num1, "-", num2, "=", subtract(num1,num2))
 
 elif choice == '3' :
   print(num1, "x", num2, "=", multiply(num1,num2))
