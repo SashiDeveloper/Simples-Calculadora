263acfe2f5732a0ded2219d000de3290ea5bc19b
SashiDeveloper <83094605+SashiDeveloper@users.noreply.github.com>
2021-07-07 22:52:42 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,36 @@
+def add(x,y) :
+  return x + y
+
+def subtract(x,y) :
+    return x - y
+
+def multiply(x,y) :
+  return x * y
+
+def divide(x,y) :
+  return x / y
+
+print("Select operation")
+print("1. ˚ ꒷Adição")
+print("2. ˚ ꒷Subtração")
+print("3. ˚ ꒷Multiplicação")
+print("4. ˚ ꒷Divisão")
+choice = input("Coloque uma das opções: ")
+
+num1 = int(input("Primeiro Número: "))
+num2 = int(input("Segundo Número: "))
+
+if choice == '1' :
+  print(num1, "+", num2, "=", add(num1,num2))
+
+elif choice == '2' :
+  print(num1, "-", num2, "=", subtraction(num1,num2))
+
+elif choice == '3' :
+  print(num1, "*", num2, "=", multiply(num1,num2))
+
+elif choice == '4' :
+  print(num1, "/", num2, "=", divide(num1,num2))
+  
+else :
+  print("Erro.")
